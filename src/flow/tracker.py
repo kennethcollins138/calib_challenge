9c144c7b6781e7f2ddf7de13cec24793c8db4ad4
Kenny Collins <kennethcollinswork@gmail.com>
2025-07-11 15:01:24 -0700

    switching devices

--- src/flow/tracker.py
+++
@@ -1,7 +0,0 @@
-class FlowTracker:
-    """
-    FlowTracker is responsible for tracking the flow of frames through the environment.
-    Essentially it is creating a short-lived 3d environment by mapping the 2d frames.
-    """
-    def __init__(self):
-        pass
